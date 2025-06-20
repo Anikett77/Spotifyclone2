57f97f715423a96154cb85f31c9c7c1c3e306bc1
Anikett77 <aniketchaturvedi83@gmail.com>
2025-06-20 23:07:56 +0530

    am

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ async function getSongs(folder) {
 
 async function displayAlbums() {
     console.log("displaying albums")
-    let a = await fetch(`/songs/`)
+    let a = await fetch("/albums.json")
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
